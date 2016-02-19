20f6e31ffa511796c617e86c4cec6c1ceb976631
Olaf Veerman <olaf.veerman@gmail.com>
2016-02-19 12:29:05 -0500

    Set production API

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -4,6 +4,7 @@ var logo = require('./logo');
  * App config for production.
  */
 module.exports = {
+  api: 'http://ocp-server.herokuapp.com',
   environment: 'production',
   consoleMessage: logo
 };
